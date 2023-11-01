496be97172b744858e5be60bc923832aa6e545e0
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-11-01 12:46:40 +0300

    feat: add header

---
+++ app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Header.css';
+
+class Header extends React.Component {
+    render() {
+        return <header>
+            <h1>Image gallery</h1>
+        </header>
+    }
+}
+
+export default Header;
